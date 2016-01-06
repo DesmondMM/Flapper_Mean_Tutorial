d3b154e0c14ca91b946e377e21f1352b4b2d4fa4
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-06 23:33:11 +0200

    Enabled Up-Voting

--- app.js
+++ app.js
@@ -5,23 +5,24 @@ app.controller('MainCtrl', [
 function($scope){
   $scope.test = 'Hello world!';
 
-  $scope.posts = [
-      {title:'post 1', upvote: 5},
-      {title:'post 2', upvote: 3},
-      {title:'post 3', upvote: 0},
-      {title:'post 4', upvote: 10},
-      {title:'post 5', upvote: 8}
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
     ];
 
-  $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
-  };
-
-  $scope.addPost = function(){
+    $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
-  };
+      $scope.link = '';
+    };
 
   $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
