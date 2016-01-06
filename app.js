b0aa1e0e637699417cbd848c6695dbadbeeb6fd2
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-06 23:58:06 +0200

    Faking Comment Data - Created Fake Comments

--- app.js
+++ app.js
@@ -39,10 +39,15 @@ function($scope, posts){
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0,
+          comments: [
+            {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+          ]
       });
+        
       $scope.title = '';
       $scope.link = '';
     };
